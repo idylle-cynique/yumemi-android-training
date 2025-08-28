f159f55557c95c3d1fe58611b790ee2a8bfcaccf
anecdoche <maltese_harvest@aol.com>
2025-08-29 07:41:21 +0900

    add: Layoutを設定

--- app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
@@ -3,9 +3,15 @@ package jp.co.yumemi.droidtraining
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,8 +19,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import jp.co.yumemi.droidtraining.ui.theme.YumemiTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,33 +42,40 @@ class MainActivity : ComponentActivity() {
                     },
                 ) { innerPadding ->
                     Box(
-//                        box→picture, text text
-//                        button, button
+                        //  box→picture, text text
+                        //  button, button
                         contentAlignment = Alignment.Center,
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(innerPadding),
                     ) {
-                        Greeting()
+                        Column(){
+                            Image(
+                                painter = painterResource(id = R.drawable.sunny),
+                                contentDescription = "A Weather Icon",
+                                modifier = Modifier.fillMaxWidth(fraction = 0.5f),
+                                contentScale = ContentScale.Fit
+                            )
+                            Row(){
+                                Button(
+                                    onClick = { }
+                                ) {
+                                    Text(
+                                        text = "Button 1"
+                                    )
+                                }
+                                Button(
+                                    onClick = { }
+                                ) {
+                                    Text(
+                                        text = "Button 2"
+                                    )
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello World!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun PictureOfWeather(modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(R.drawable.sunny),
-        contentDescription = "A Weather Icon"
-    )
 }
