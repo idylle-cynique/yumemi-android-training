31d8771648f5e1eb2ad66831452a9c9ef62d615a
anecdoche <kimitaka.nishimura@theport.jp>
2026-04-02 13:21:51 +0900

    build(gradle): hiltに関する依存を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,16 +22,17 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = JvmTarget.JVM_1_8.target
-    }
+    kotlinOptions { jvmTarget = JvmTarget.JVM_1_8.target }
 }
 
 dependencies {
@@ -45,4 +46,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
 }
